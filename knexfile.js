ef3770466dba0070f4ef7fa1317cfbe870499f0e
JonathanChen <jonny29009@yahoo.com>
2020-01-26 23:27:22 -0800

    create table for user

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/user-auth.db3'
+    },
+    useNullAsDefault: true,
+    migration: {
+      directory: './data/migrations'
+    }
+  },
+};
