cf3fc34b58c21f79a48c53be6c6d745f8b8e8be8
JonathanChen <jonny29009@yahoo.com>
2020-01-26 23:54:27 -0800

    rework project structure, add seed in knexfile

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.json({ message: 'W15 Monday & Tuesday server up and running.'})
